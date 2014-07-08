4a52c5e2164f4dcaa3e7b8163d1232e525277ff7
Alan Hardman <alanaktion@gmail.com>
2014-07-07 19:06:25 -0600

    Adding code for future improvements to log parsing

--- inc/mclogparse.inc.php
+++ inc/mclogparse.inc.php
@@ -36,6 +36,75 @@ function mclogparse($str) {
 
 }
 
+// Parses text with control codes and returns what may be usable HTML.
+// Credit: http://theperfectbeast.blogspot.com.es/2013/10/minecraft-server-log-web-interface.html
+function mclogparse2($str) {
+
+	$lines = explode("\n", $str);
+
+	foreach($lines as &$line) {
+		/* Remap Problem Characters */
+		$line = preg_replace("/</", "&lt;", $line);
+		$line = preg_replace("/>/", "</span>&gt;", $line);
+		$date = substr($line, 0, 10);
+		if ($date == $last) {
+			/* Remove Unrequired Formatting Codes */
+			$line = str_replace("[m", "", $line);
+			$line = str_replace("[21m", "", $line);
+			$line = str_replace("[3m", "", $line);
+
+			/* Split Log Line Into Sections to Using First Formatting Code Style */
+			$segarray = preg_split( '/(\[0|\[m)/', $line );
+			for ($i = 1; $i < count($segarray); ++$i) {
+				/* Do Replace to Add Styled Spans */
+				if (preg_match('/;\d{2};\d+m/', $segarray[$i])) {
+				$segarray[$i] = preg_replace("/;30/", "<span class='black", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;31/", "<span class='red", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;32/", "<span class='green", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;33/", "<span class='gold", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;34/", "<span class='blue", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;35/", "<span class='purple", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;36/", "<span class='aqua", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;37/", "<span class='gray", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;22m/", "'>", $segarray[$i]);
+				$segarray[$i] = preg_replace("/;1m/", " bold'>", $segarray[$i]);
+				$segarray[$i] .= "</span>";
+			}
+		}
+
+		/* Rejoin Then Split Log Line Using Second Formatting Code Style */
+		$line = implode("", $segarray);
+		$segarray = preg_split('/§/', $line);
+
+		for ($i = 1; $i < count($segarray); ++$i) {
+			/* Do Replace to Add Styled Spans */
+			$segarray[$i] = preg_replace("/^0/", "<span class='black'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^1/", "<span class='blue'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^2/", "<span class='green'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^3/", "<span class='aqua'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^4/", "<span class='red'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^5/", "<span class='purple'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^6/", "<span class='gold'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^7/", "<span class='gray'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^8/", "<span class='gray'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^9/", "<span class='blue'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^a/", "<span class='green'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^b/", "<span class='aqua'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^c/", "<span class='red'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^d/", "<span class='purple'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^e/", "<span class='gold'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^f/", "<span class='black'>", $segarray[$i]);
+			$segarray[$i] = preg_replace("/^r/", "<span class='black'>", $segarray[$i]);
+			$segarray[$i] .= "</span>";
+		}
+
+		$line = implode("", $segarray);
+	}
+
+	return implode("\n", $lines);
+
+}
+
 // Strips control codes from log
 function mclogclean($str) {
 	$str = preg_replace("/\x1B\[([0-9]+;?([0-9]+;)?(1|22)?)?m/", "", $str);
