7ac99a9b61bed3e4453378f5959b5f34448613d9
Alan Hardman <ahardman@thrivelife.com>
2015-06-26 08:44:03 -0600

    Minor compatibility fix to top.php

--- inc/top.php
+++ inc/top.php
@@ -1,26 +1,13 @@
-<?php
-session_start();
-
-	if ($_SESSION['user']) {
-
-	if (!$user = user_info($_SESSION['user'])) {
-		// User does not exist, redirect to login page
-		header('Location: .');
-		exit('Not Authorized');
-	}
-	}
-
-?>
 <div class="navbar navbar-inverse navbar-fixed-top">
 	<div class="navbar-inner">
 		<a class="brand" href="dashboard.php">MC<strong>Host</strong>Panel</a>
 		<ul class="nav pull-right">
-<?php if($_SESSION['is_admin'] || $user['role']=='admin') { ?>
-			<li><a href="admin.php">Administration</a></li>
-<?php } ?>
+			<?php if(!empty($_SESSION['is_admin']) || $user['role']=='admin') { ?>
+				<li><a href="admin.php">Administration</a></li>
+			<?php } ?>
 
 			<li><a href='userProfile.php'>Logged in as <strong><?php echo $user['user'];?></strong></a></a></li>
-			
+
 			<li><img src="http://alanaktion.net/mcface.php?user=<?php echo urlencode($user['user']); ?>" alt="<?php echo $user['user']; ?>"></li>
 			<li><a href="./?logout"><i class="icon-off icon-white"></i> Log Out</a></li>
 		</ul>
