6359414262ff700caa62f55c83374a2296be9131
Alan Hardman <alanaktion@gmail.com>
2015-06-28 12:47:17 -0600

    Minor fix to getFace.php

--- inc/getFace.php
+++ inc/getFace.php
@@ -1,7 +1,11 @@
 <?php
-include ('getminecraftprofile.php' )?>
-<?php 
+require 'getminecraftprofile.php';
+
 $profile = ProfileUtils::getProfile($_GET['username']);
+if($profile === null) {
+	exit('Failed to find profile ' . $_GET['username']);
+}
 $result = $profile->getFace();
+
 header('Content-type: image/png');
 echo $result;
