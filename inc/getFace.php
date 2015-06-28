f65b32849a720954b63d677cd8b9d27359f79970
Alan Hardman <alanaktion@gmail.com>
2015-06-28 13:03:41 -0600

    New, untested getFace!

--- inc/getFace.php
+++ inc/getFace.php
@@ -1,11 +1,36 @@
 <?php
-require 'getminecraftprofile.php';
 
-$profile = ProfileUtils::getProfile($_GET['username']);
-if($profile === null) {
-	exit('Failed to find profile ' . $_GET['username']);
+$username = $_GET['username'];
+if(empty($username)) exit;
+
+$filename = 'skintmp/' . md5($username) . '.png';
+if(file_exists($filename) && time()-filemtime($filename) > 3600*24*3) { // cached file exists, is newer than 3 days
+	$raw = file_get_contents($filename);
+} else {
+	$url = 'https://api.mojang.com/profiles/minecraft';
+	$options = array('header' => "Content-type: application/json\r\n", 'method' => 'POST', 'content' => json_encode(array($username)));
+	$context = stream_context_create($options);
+	$profile = json_decode(file_get_contents($url, false, $context));
+	if(empty($profile->id)) exit;
+
+	$data = json_decode(file_get_contents('https://sessionserver.mojang.com/session/minecraft/profile/' . $profile->id));
+	$properties = json_decode(base64_decode($data->properties[0]));
+	$raw = file_get_contents($properties->textures->SKIN->url);
+
+	if(!is_dir('mktmp')) mkdir('skintmp');
+	file_put_contents($filename, $raw);
 }
-$result = $profile->getFace();
+
+$size = isset($_GET['size']) ? max(8, min(250, $_GET['size'])) : 48;
+
+$im = imagecreatefromstring($raw);
+$av = imagecreatetruecolor($size, $size);
+imagecopyresized($av, $im, 0, 0, 8, 8, $size, $size, 8, 8); // Face
+imagecolortransparent($im, imagecolorat($im, 63, 0)); // Black Hat Issue
+imagecopyresized($av, $im, 0, 0, 40, 8, $size, $size, 8, 8); // Accessories
 
 header('Content-type: image/png');
-echo $result;
+echo imagepng($av);
+
+imagedestroy($im);
+imagedestroy($av);
